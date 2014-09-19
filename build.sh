d777ab6b0e0326a13c2d0436d469063d6586cbac
c9s <yoanlin93@gmail.com>
2014-09-20 00:22:53 +0900

    Fix build script

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ set -e
 MYSQL_CONN_C_DIR=$PWD/mysql-connector-c-6.1.5-osx10.7-x86
 prefix=/opt/local
 PATH=$prefix/libexec/elftoolchain:$PATH
+PATH=$prefix/libexec/ocaml3:$PATH
 git submodule update --init --recursive --force
 
+# workable revision: 01228273b8cf709aacbd3df1c51b1e690ecebac8
 time cmake . \
     -Wno-dev \
     -DCMAKE_CXX_COMPILER=$(which g++-mp-4.8) \
@@ -12,6 +14,7 @@ time cmake . \
     -DCMAKE_ASM_COMPILER=$(which gcc-mp-4.8) \
     -DLIBIBERTY_LIB=$prefix/lib/gcc48/x86_64/libiberty.a \
     -DCMAKE_PREFIX_PATH=$prefix \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_INCLUDE_PATH="$prefix/include/gcc48/c++/parallel:$prefix/include:/usr/include" \
     -DCMAKE_LIBRARY_PATH="$prefix/lib:/usr/lib" \
     -DLIBDWARF_LIBRARIES=$prefix/lib/elftoolchain/libdwarf.3.dylib \
