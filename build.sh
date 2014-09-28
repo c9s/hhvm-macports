e3f8838181b0a4b32f520119b827649fff66791d
c9s <yoanlin93@gmail.com>
2014-09-28 19:22:13 +0800

    Add HHVM 2.4 port

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
-MYSQL_CONN_C_DIR=$PWD/mysql-connector-c-6.1.5-osx10.7-x86
 prefix=/opt/local
-PATH=$prefix/libexec/elftoolchain:$PATH
-# PATH=$prefix/libexec/ocaml3:$PATH
-
-find . -type f -iname "CMakeCache.txt" -delete
-find . -type d -iname "CMakeFiles" | xargs -I{} rm -rf {}
-# workable revision: 01228273b8cf709aacbd3df1c51b1e690ecebac8
 time cmake . \
     -Wno-dev \
     -DCMAKE_CXX_COMPILER=$(which g++-mp-4.8) \
@@ -32,21 +25,13 @@ time cmake . \
     -DLibYaml_INCLUDE_DIRS=$prefix/include \
     -DMYSQL_INCLUDE_DIR=$prefix/include/mysql55/mysql \
     -DMYSQL_LIB=$prefix/lib/mysql55/mysql/libmysqlclient.dylib \
-    -DBOOST_INCLUDEDIR=$prefix/include \
-    -DBOOST_LIBRARYDIR=$prefix/lib \
     -DBoost_USE_STATIC_LIBS=ON \
+    -DBoost_DEBUG=ON \
+    -DBoost_LIBRARY_DIR=${prefix}/boost-gcc/lib \
+    -DBoost_INCLUDE_DIR=${prefix}/boost-gcc/include \
+    -DBOOST_ROOT=${prefix}/boost-gcc \
+    -DBOOST_INCLUDEDIR=${prefix}/boost-gcc/include \
+    -DBOOST_LIBRARYDIR=${prefix}/boost-gcc/lib \
     -DPCRE_INCLUDE_DIR=$prefix/include \
     -DPCRE_LIBRARY=$prefix/lib/libpcre.dylib  
 make -j4
-
-    # -DCURL_INCLUDE_DIR=$prefix/include \
-    # -DCURL_LIBRARY=$prefix/lib/libcurl.dylib
-    # -DREADLINE_INCLUDE_DIR=$prefix/include \
-    # -DREADLINE_LIBRARY=$prefix/lib/libreadline.dylib \
-
-
-    # -DMYSQL_INCLUDE_DIR=$MYSQL_CONN_C_DIR/include \
-    # -DMYSQL_LIB=$MYSQL_CONN_C_DIR/lib/libmysqlclient.dylib  \
-
-make -j4
-    # sudo ln -s freetype2 freetype
